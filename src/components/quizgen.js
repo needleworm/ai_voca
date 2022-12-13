bf042335aaf5e03dae3284857f44fa575ab70e57
needleworm <bhban@kakao.com>
2022-12-13 17:57:22 +0900

    .

--- src/components/quizgen.js
+++ src/components/quizgen.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import './websites.css';
 
 
+const hashCode = function(s){
+  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+}
+
 class Quizgen extends Component {
   constructor(props){
     super(props);
@@ -21,6 +25,7 @@ class Quizgen extends Component {
 
   drawWebsites(){
     const passwd = "UYxe-JEHo-9G1T-DAOp"
+    var passing = hashCode(passwd) === -1693770398
 
     return (
       <div className="websitesContainer">
