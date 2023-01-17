b2c24b8384b331c05398b39c4b338b87b358ed15
Ban Byeong-Hyeon <bhban@Banui-MacBookAir.local>
2023-01-17 16:31:00 +0900

    .

--- src/components/books.tsx
+++ src/components/books.tsx
@@ -9,15 +9,17 @@ function Books() {
   const yes24Icon = <img src='https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/stores/yes24.png' className="bookStoreIcon" alt="yes24"/>
 
   const drawBooks = () => {
+    
+    
     let bookSection1 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/19.jpg" alt="book1" className="bookCoverImage"/>
-      <h5>AI족집게 수능영단어</h5>
-      <p className="bookDescription">2023.2. 생능북스</p>
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/19.jpg" alt="book19" className="bookCoverImage"/>
+      <h5><a href="https://needleworm.github.io/ai_voca" target="_blank"  rel="noreferrer">AI족집게 수능영단어 <i className="fas fa-external-link-alt"></i></a></h5>
+      <p className="bookDescription">2023.01. 생능북스</p>
       <ul className="list-inline">
-        <li><a href="#" target="_blank" rel="noreferrer">{alladinIcon}</a></li>
-        <li><a href="#" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
-        <li><a href="#" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
-        <li><a href="#" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
+        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=8970506667" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
+        <li><a href="https://product.kyobobook.co.kr/detail/S000200661730" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
+        <li><a href="https://search.shopping.naver.com/book/catalog/37153902621" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
+        <li><a href="http://www.yes24.com/Product/Goods/116904678" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
       </ul>
     </div>
 
